07876c13c1b7570e7d06358e22a871241b0bdb9f
iimagii <mag_1@ukr.net>
2023-01-25 18:22:35 +0200

    делаю роутинг

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import './App.css'
+import { Layout } from './components/Layout'
+import { MainPage } from './pages/MainPage'
+import { PostsPage } from './pages/PostsPage'
+
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
-   return <div className='text-2xl text-red-600'>TEXT</div>
+   return (
+      <Layout>
+         <Routes>
+            <Route path='/' element={<MainPage />} />
+            <Route path='/posts' element={<PostsPage />} />
+         </Routes>
+      </Layout>
+   )
 }
 
 export default App

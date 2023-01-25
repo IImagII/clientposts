1bbf76f4a33580f784e0ef5b09c9f64f50fac535
iimagii <mag_1@ukr.net>
2023-01-25 18:31:03 +0200

    доделал роутинг

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import './App.css'
 import { Layout } from './components/Layout'
 import { MainPage } from './pages/MainPage'
 import { PostsPage } from './pages/PostsPage'
+import { PostPage } from './pages/PostPage'
+import { AddPostPages } from './pages/AddPostPages'
+import { LoginPage } from './pages/LoginPage'
+import { RegisterPage } from './pages/RegisterPage'
 
 import { Route, Routes } from 'react-router-dom'
 
@@ -10,7 +13,11 @@ function App() {
       <Layout>
          <Routes>
             <Route path='/' element={<MainPage />} />
-            <Route path='/posts' element={<PostsPage />} />
+            <Route path='posts' element={<PostsPage />} />
+            <Route path=':id' element={<PostPage />} />
+            <Route path='new' element={<AddPostPages />} />
+            <Route path='login' element={<LoginPage />} />
+            <Route path='register' element={<RegisterPage />} />
          </Routes>
       </Layout>
    )

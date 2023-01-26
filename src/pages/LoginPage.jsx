c996bd665a027b64f99d6c2a5403a8882f9fc809
iimagii <mag_1@ukr.net>
2023-01-26 20:59:54 +0200

    делаю логику регистрации

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const LoginPage = () => {
    return (
@@ -15,14 +16,28 @@ export const LoginPage = () => {
                className='mt-1 text-black w-full rounded-lg bg-gray-400 border py-1 px-2 text-xs outline-none placeholder:text-gray-700'
             />
          </label>
-         <label className='text-xs text-gray-400'>
+         <label className='text-xs text-gray-400 '>
             Password:
             <input
-               type='text'
+               type='password'
                placeholder='Password'
                className='mt-1 text-black w-full rounded-lg bg-gray-400 border py-1 px-2 text-xs outline-none placeholder:text-gray-700'
             />
          </label>
+         <div className='flex gap-8 justify-center mt-4'>
+            <button
+               className='flex justify-center items-center text-xs bg-gray-600 text-white rounded-sm py-2 px-4'
+               type='submit'
+            >
+               Войти
+            </button>
+            <Link
+               to='/register'
+               className='flex justify-center items-center text-xs text-white'
+            >
+               Зарегестрироваться
+            </Link>
+         </div>
       </form>
    )
 }

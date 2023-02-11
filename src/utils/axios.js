549d12ed391228370dd7e4fb85620d17c943325c
iimagii <mag_1@ukr.net>
2023-02-11 21:01:04 +0200

    deploy 1.02

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios'
 const instance = axios.create({
    // baseURL: 'http://localhost:3002/api',
 
-   baseURL: 'server-posts-full-stack-production.up.railway.app:3002/api',
+   baseURL: 'http://server-posts-full-stack-production.up.railway.app:3002/api',
 })
 //делам так чтобы при каждом запросе мы отсылали токен если он у нас есть
 instance.interceptors.request.use(config => {

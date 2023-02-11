c7950e41604480a50854a747d02bf4cef9444629
iimagii <mag_1@ukr.net>
2023-02-11 20:37:14 +0200

    деплой

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -2,7 +2,9 @@ import axios from 'axios'
 
 //делаем расширение для axios
 const instance = axios.create({
-   baseURL: 'http://localhost:3002/api',
+   // baseURL: 'http://localhost:3002/api',
+
+   baseURL: ' server-posts-full-stack-production.up.railway.app/api',
 })
 //делам так чтобы при каждом запросе мы отсылали токен если он у нас есть
 instance.interceptors.request.use(config => {
